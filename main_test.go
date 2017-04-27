609c7c5905805a3b9809e329001609eb1866961b
Alexander Tkachuk <mail@tksasha.me>
2017-04-27 11:01:30 +0300

    add tests for Results.Len(), Results.Less() and Results.Swap()

--- main_test.go
+++ main_test.go
@@ -6,10 +6,41 @@ import (
   "github.com/stretchr/testify/assert"
 
   . "github.com/tksasha/one"
+
+  "fmt"
 )
 
-func TestResultsLen(t *testing.T) {
-  results := make(Results, 10, 59)
+var _ = fmt.Println // DELME
 
-  assert.Equal(t, 10, results.Len())
+func NewResults() (results Results) {
+  for _, letter := range "Hello" {
+    results.Append(letter)
+  }
+
+  return
+}
+
+func TestResultsLen(t *testing.T) {
+  results := NewResults()
+
+  assert.Equal(t, 4, results.Len())
+}
+
+func TestResultsLess(t *testing.T) {
+  results := NewResults()
+
+  assert.True(t, results.Less(2, 3))
+}
+
+func TestResultsSwap(t *testing.T) {
+  results := NewResults()
+
+  results.Swap(1, 2)
+
+  expectation := Results{
+    []int{ 108, 2 },
+    []int{ 101, 1 },
+  }
+
+  assert.Equal(t, expectation, results[1:3])
 }
